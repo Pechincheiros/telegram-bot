01fcecf5179e68b6cb8662eb3fd4e58f2c5bb195
Ryan Castro <38301142+castrorry@users.noreply.github.com>
2020-11-26 11:50:44 -0300

    Initial commit in branch develop

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default (request: NowRequest, response: NowResponse) =>
-  response.status(200).json({ ok: true });
+  response.status(200).json({ ok: 'develop' });
